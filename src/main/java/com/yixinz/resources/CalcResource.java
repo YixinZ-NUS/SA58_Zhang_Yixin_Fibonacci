8e512a8512a4cde2e1bccbd6175c50d584135030
YixinZ-NUS <zyx83028016zyx@gmail.com>
2024-07-14 19:23:14 +0800

    bugfix: backend-side forgot to merge to main; learned something

--- src/main/java/com/yixinz/resources/CalcResource.java
+++ src/main/java/com/yixinz/resources/CalcResource.java
@@ -2,42 +2,32 @@ package com.yixinz.resources;
 
 import com.codahale.metrics.annotation.Timed;
 import com.yixinz.api.CoinDenominations;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import com.yixinz.api.DenoRequest;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.DoubleStream;
 
 @Path("/find")
+@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 // for now, use find?amt=10000
 public class CalcResource {
-    @NotEmpty
-    private double[] coinsToUse;
-    //TODO: change way to pass value, utilizing configuration, 'Getting Started- Registering A Resource'
-
-    //TODO: Implicitly pass targetAmt
-    //private double targetAmt;
     private final AtomicLong counter;
 
     public CalcResource() {
-
-        //this.coins = coins;
-        this.coinsToUse= new double[]{0.01, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 50, 100, 1000};
-        // TODO: change later to fit user's choice; how to initialize coins?
-        // *100 to all values to avoid 0.3000000004 issue
         this.counter = new AtomicLong();
     }
 
-    @GET
+    @POST
     @Timed
-    public CoinDenominations WrapRes(@QueryParam("amt") double targetAmt, double[] coins) {
-        coins = this.coinsToUse; // to change later
+    public CoinDenominations WrapRes(@Valid DenoRequest request) {
+        double targetAmt = request.getTargetAmt();
+        List<Double> coins = request.getCoinsToUse();
+        System.out.println("Received request: " + request);
+        // *100 to all values to avoid 0.3000000004 issue
         double[] res = Calc((int)(targetAmt*100),getCoinsIntForCalc(coins)).stream().
                 mapToDouble(Integer::doubleValue).
                 map(x->x/100).
@@ -52,47 +42,47 @@ public class CalcResource {
     space complexity O(targetAmt)
     */
     public ArrayList<Integer> Calc(int targetAmt, int[] coins){
-                int max = targetAmt + 1;
-                //dp[i] holds the min coins required for amount i.
-                //dp[targetAmt] contains the minimum number of coins required to make up the target amount.
-                //prev[i] holds the previous state for optimal solution at i
-                int[] dp = new int[targetAmt + 1];
-                int[] prev = new int[targetAmt + 1];
+        int max = targetAmt + 1;
+        //dp[i] holds the min coins required for amount i.
+        //dp[targetAmt] contains the minimum number of coins required to make up the target amount.
+        //prev[i] holds the previous state for optimal solution at i
+        int[] dp = new int[targetAmt + 1];
+        int[] prev = new int[targetAmt + 1];
 
-                // Initialize dp array with max value except dp[0] which is 0
-                Arrays.fill(dp, max);
-                dp[0] = 0;
+        // Initialize dp array with max value except dp[0] which is 0
+        Arrays.fill(dp, max);
+        dp[0] = 0;
 
-                for (int i = 1; i <= targetAmt; i++) {
-                    for (int coin : coins) {
-                        if (coin <= i) {
-                            if (dp[i] > dp[i - coin] + 1) {
-                                //If current coin can be used and achieves better result,
-                                //Update the minimum coins required and the previous state
-                                dp[i] = dp[i - coin] + 1;
-                                prev[i] = i - coin;
-                            }
-                        }
+        for (int i = 1; i <= targetAmt; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    if (dp[i] > dp[i - coin] + 1) {
+                        //If current coin can be used and achieves better result,
+                        //Update the minimum coins required and the previous state
+                        dp[i] = dp[i - coin] + 1;
+                        prev[i] = i - coin;
                     }
                 }
-
-                if (dp[targetAmt] > targetAmt) {
-                    return new ArrayList<>();
-                }
-                //Reconstructing the list of denominations, backtrack from targetAmt to 0
-
-                ArrayList<Integer> result = new ArrayList<>();
-                while (targetAmt > 0) {
-                    result.add(targetAmt - prev[targetAmt]);// Add the denomination used
-                    targetAmt = prev[targetAmt];// Move to the previous state
-                }
-            return result;
+            }
         }
 
-    public int[] getCoinsIntForCalc(double[] coins){
-        return DoubleStream.of(coins).
+        if (dp[targetAmt] > targetAmt) {
+            return new ArrayList<>();
+        }
+        //Reconstructing the list of denominations, backtrack from targetAmt to 0
+
+        ArrayList<Integer> result = new ArrayList<>();
+        while (targetAmt > 0) {
+            result.add(targetAmt - prev[targetAmt]);// Add the denomination used
+            targetAmt = prev[targetAmt];// Move to the previous state
+        }
+        return result;
+    }
+
+    public int[] getCoinsIntForCalc(List<Double> coins){
+        return coins.stream().
                 map(x->x*100).
-                mapToInt(x->(int)x).
+                mapToInt(Double::intValue).
                 toArray();
     }
 }
